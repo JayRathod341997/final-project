653b5210271a670cbf557a4b541abec1ed5bfbe8
Jay Rathod <rathodjay4397@gmail.com>
2024-12-16 02:14:20 +0530

    new

--- tumor_detection.py
+++ tumor_detection.py
@@ -16,9 +16,19 @@ def preprocess_image(image):
     return image
 
 
+def tumor_detection_module():
+    model = load_remote_model()
+    st.title("🧠 Brain Tumor Detection & Appointment Booking App")
+    st.write("🔍 Choose an option below to either detect a brain tumor or book an appointment for consultation.")
 
-def upload_file_module(model):
-    uploaded_file = st.file_uploader(
+    # Sidebar for feature selection
+    option = st.radio("Select an option",
+                    ("🧠 Brain Tumor Detection", "📅 Book an Appointment"))
+
+    if option == "🧠 Brain Tumor Detection":
+        st.subheader(
+            "🔬 Upload an MRI image to check for the presence of a brain tumor.")
+        uploaded_file = st.file_uploader(
         "Choose an MRI image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
@@ -38,19 +48,5 @@ def upload_file_module(model):
         else:
             st.success(
                 "✅ No tumor detected. Keep up with regular health check-ups to stay healthy! 💪")
-
-def tumor_detection_module():
-    model = load_remote_model()
-    st.title("🧠 Brain Tumor Detection & Appointment Booking App")
-    st.write("🔍 Choose an option below to either detect a brain tumor or book an appointment for consultation.")
-
-    # Sidebar for feature selection
-    option = st.radio("Select an option",
-                    ("🧠 Brain Tumor Detection", "📅 Book an Appointment"))
-
-    if option == "🧠 Brain Tumor Detection":
-        st.subheader(
-            "🔬 Upload an MRI image to check for the presence of a brain tumor.")
-        upload_file_module(model)
     elif option == "📅 Book an Appointment":
         book_appointment()
